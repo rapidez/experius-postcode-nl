80e48224bb9611ee92544d243cd8849fb161a4ed
Roy Duineveld <royduin@users.noreply.github.com>
2021-08-03 11:38:09 +0000

    Apply fixes from StyleCI

--- src/ExperiusPostcodeNLServiceProvider.php
+++ src/ExperiusPostcodeNLServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Rapidez\ExperiusPostcodeNL;
 
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class ExperiusPostcodeNLServiceProvider extends ServiceProvider
