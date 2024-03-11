38edf0c17a0c3ebe608700750f424081fcaa6807
indy koning <indy@justbetter.nl>
2024-03-11 13:39:33 +0100

    Listen on postcode-change event

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,1 +1,42 @@
-import './components'
+import { set, useDebounceFn, useMemoize } from "@vueuse/core";
+
+
+document.addEventListener('turbo:load', function () {
+    window.app.$on('postcode-change', useDebounceFn(updateAddressFromExperiusPostcode, 100));
+})
+
+const getAddressFromExperiusPostcode = useMemoize(
+    async function (postcode, housenumber, housenumberAddition = '') {
+        return magentoGraphQL(
+            'query postcode($postcode: String! $houseNumber: String! $houseNumberAddition: String) { postcode(postcode: $postcode houseNumber: $houseNumber houseNumberAddition: $houseNumberAddition) { street houseNumber houseNumberAddition postcode city province houseNumberAdditions } }',
+            {
+                postcode: postcode,
+                houseNumber: housenumber,
+                houseNumberAddition: housenumberAddition
+            }
+        )
+    }
+)
+
+async function updateAddressFromExperiusPostcode(address) {
+    if ((address?.country_id || address?.country_code) != 'NL') {
+        return
+    }
+
+    if(!address.postcode || !(address?.housenumber || address.street[1])) {
+        return
+    }
+
+    let response = await getAddressFromExperiusPostcode(address.postcode, address?.housenumber || address.street[1])
+
+    if (!response.data?.postcode?.city || !response.data?.postcode?.street) {
+        if (response.data?.postcode?.error == "Postcode not found") {
+            set(address, 'city', '')
+            set(address.street, 0, '')
+        }
+        return
+    }
+
+    set(address, 'city', response.data.postcode.city)
+    set(address.street, 0, response.data.postcode.street)
+}
--- src/ExperiusPostcodeNLServiceProvider.php
+++ src/ExperiusPostcodeNLServiceProvider.php
@@ -8,10 +8,6 @@ class ExperiusPostcodeNLServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'experius-postcode-nl');
 
-        $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/experius-postcode-nl'),
-        ], 'views');
     }
 }
